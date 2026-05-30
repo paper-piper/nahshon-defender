b6c7b9d21f27507746c3acb9556befca513b9490
paper-piper <113584610+paper-piper@users.noreply.github.com>
2026-05-30 12:14:38 +0300

    added setup.sh correct shabeng

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 git config core.hooksPath .githooks # Redirect Git's hook lookup to our version-controlled directory.
 chmod +x .githooks/pre-commit # Guarantee the hook is executable.
